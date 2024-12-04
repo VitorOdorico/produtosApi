07346b32cdf755e00e52e9bca24d609475e244a5
Vitor Emanuel Odorico Neto Costa <VCosta@solucaonetwork.com>
2024-12-03 21:49:31 -0300

    get put del post prontos

---
+++ src/main/java/com/produtosapi/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,50 @@
+package com.produtosapi.produtosapi.controller;
+
+import com.produtosapi.produtosapi.model.Produto;
+import com.produtosapi.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+    // o repositorio(instancia da entidade)
+    private ProdutoRepository produtoRepository;
+    //  controlador
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto) {
+        System.out.println("Produto recebido com sucesso: " + produto.toString());
+       //cria uma vareavel para poder instanciar um recurso nativo que e o uniqueID, chamando o metodo e o transformando em uma string pois nossa entidade no banco e uma string
+        var id = UUID.randomUUID().toString();
+        // aqui ee setado o valor no produto no atributo id
+        produto.setId(id);
+        // aqui e salvo o valor recebido via URL playload do postman no repository para salvar no banco
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    @GetMapping("/{id}")
+    public Produto buscarPorId(@PathVariable("id") String id) {
+        // se passar um id existente ele retornaria o id, se caso o contrario nao retornaria por isso do optional
+        Optional<Produto> produto = produtoRepository.findById(id);
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void remover(@PathVariable("id") String id) {
+        produtoRepository.deleteById(id);
+    }
+    @PutMapping("/{id}")
+    public void atualizar(@PathVariable("id")String id,@RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+}
---
+++ src/main/java/com/produtosapi/produtosapi/model/Produto.java
@@ -0,0 +1,77 @@
+package com.produtosapi.produtosapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Produto {
+    @Column
+    @Id
+    private String id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String descricao;
+
+    @Column
+    private double preco;
+
+    @Column
+    private int quantidade;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
